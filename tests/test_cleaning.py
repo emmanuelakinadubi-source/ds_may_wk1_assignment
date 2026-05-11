7337661aa80b004ad3b1c38bd0487e9f1c521746
emmanuelakinadubi-source <emmanuel.akin.adubi@gmail.com>
2026-05-11 16:24:50 +0100

    tested test_cleaning.py

--- tests/test_cleaning.py
+++ tests/test_cleaning.py
@@ -10,31 +10,32 @@ from src.utils.data_check import DataCheck
 
 def test_total_salary_validation():
     df = pd.DataFrame({
-        "Age": [25],
-        "Gender": ["Male"],
-        "Profession": ["Salaried"],
-        "Marital_status": ["Single"],
-        "Education": ["Graduate"],
-        "No_of_Dependents": [1],
-        "Personal_loan": ["Yes"],
-        "House_loan": ["No"],
-        "Partner_working": ["Yes"],
-        "Salary": [50000],
-        "Partner_salary": [20000],
-        "Total_salary": [1],
-        "Price": [25000],
+        "age": [25],
+        "gender": ["Male"],
+        "profession": ["Salaried"],
+        "marital_status": ["Single"],
+        "education": ["Graduate"],
+        "no_of_dependents": [1],
+        "personal_loan": ["Yes"],
+        "house_loan": ["No"],
+        "partner_working": ["Yes"],
+        "salary": [50000],
+        "partner_salary": [20000],
+        "total_salary": [1],
+        "price": [25000],
         "make": ["Hatchback"]
     })
 
     cleaned = DataCleaner.validate_salary_total(df)
-    assert cleaned["Total_salary"].iloc[0] == 70000
+    assert cleaned["total_salary"].iloc[0] == 70000
 
 
 def test_salary_consistency_detects_no_mismatch():
     df = pd.DataFrame({
-        "Salary": [50000],
-        "Partner_salary": [20000],
-        "Total_salary": [70000]
+        "salary": [50000],
+        "partner_salary": [20000],
+        "total_salary": [70000]
     })
+
     mismatch = DataCheck.salary_consistency(df)
     assert mismatch.empty
